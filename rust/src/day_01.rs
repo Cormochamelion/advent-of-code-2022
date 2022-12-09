8402a3f41537b7046ab54fdb6e677957f9fb82df
Cormochamelion <70096814+Cormochamelion@users.noreply.github.com>
2022-12-09 14:43:18 +0000

    Finish day_01.rs part 1

--- rust/src/day_01.rs
+++ rust/src/day_01.rs
@@ -14,10 +14,32 @@ fn main() {
     };
 
     // Read the file contents into a string, returns `io::Result<usize>`
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
+    let mut calories = String::new();
+    match file.read_to_string(&mut calories) {
         Err(why) => panic!("couldn't read {}: {}", display, why),
-        Ok(_) => print!("{} contains:\n{}", display, s),
+        Ok(s) => s,
+    };
+
+    let mut sum: u32 = 0;
+    let mut best_sum: u32 = 0;
+
+    for line in calories.lines() {
+        if line == "" {
+            if sum > best_sum {
+                best_sum = sum;
+            }
+            sum = 0;
+            continue;
+        }
+
+        // println!("{}", line);
+
+        sum += match line.parse::<u32>() {
+            Err(why) => panic!("Couldn't parse {} to type u32: {}", line, why),
+            Ok(calorie) => calorie,
+        };
     }
+
+    println!("Maximum calories: {best_sum}")
 }
 
