ea95cfafd8ba69ec1244f88123818e09e2bb5180
Cormochamelion <70096814+Cormochamelion@users.noreply.github.com>
2022-12-10 11:00:53 +0000

    Add utils module with fun for file reading

---
+++ rust/src/utils.rs
@@ -0,0 +1,22 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
+pub fn string_from_file(path: &Path) -> String {
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut content = String::new();
+    match file.read_to_string(&mut content) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(s) => s,
+    };
+
+    content
+}
